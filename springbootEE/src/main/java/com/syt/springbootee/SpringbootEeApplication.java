be5d4a9a9411bf5546ea7d734a1a0e7e17fa3cbb
lingnian <sytsnb@gmail.com>
2022-10-12 02:16:19 +0800

    springbootEE

---
+++ springbootEE/src/main/java/com/syt/springbootee/SpringbootEeApplication.java
@@ -0,0 +1,13 @@
+package com.syt.springbootee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEeApplication.class, args);
+    }
+
+}
---
+++ springbootEE/src/test/java/com/syt/springbootee/SpringbootEeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syt.springbootee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
