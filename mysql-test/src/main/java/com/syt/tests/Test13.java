1972d6339f7b6a7fd26eef28cb942885ae270ac5
lingnian <sytsnb@gmail.com>
2022-10-14 21:21:52 +0800

    Map.iter

--- mysql-test/src/main/java/com/syt/tests/Test13.java
+++ mysql-test/src/main/java/com/syt/tests/Test13.java
@@ -1,6 +1,7 @@
 package com.syt.tests;
 
-import java.util.HashMap;
+import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * @author sytsnb@gmail.com
@@ -8,17 +9,41 @@ import java.util.HashMap;
  */
 public class Test13 {
     public static void main(String[] args) {
-        String s = "qwe";
-        int length = s.length();
-        char c = s.charAt(0);
-        System.out.println("c = " + c);
-        int r = s.indexOf('r');
-        System.out.println("r = " + r);
 
-        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+        Map<Object, Object> map = new HashMap<>();
+        map.put(1, "123");
+        map.put(2, "456");
+        map.put(3, "789");
+        System.out.println("map.size() =" + map.size());
 
-        for (int i = 0; i < objectObjectHashMap.size(); i++) {
+        System.out.println("====通过Map.keySet遍历key和value===========================");
+        //1.通过Map.keySet遍历key和value
+        for (Object key : map.keySet()) {
+            Object value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
 
+        System.out.println("==通过Map.entrySet,,,forEach===========================");
+        Set<Map.Entry<Object, Object>> entries = map.entrySet();
+        //2.Set通过Map.entrySet,,,forEach
+        entries.forEach(System.out::println);
+
+        System.out.println("==通过Map.entrySet使用iterator遍历key和value=========================");
+        //3.通过Map.entrySet使用iterator遍历key和value
+        Iterator<Map.Entry<Object, Object>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Object, Object> entry = iterator.next();
+            System.out.println(entry.getKey() + "=" + entry.getValue());
+        }
+
+        System.out.println("==Map.entrySet使用iterator遍历key和value..===推荐,尤其是容量大时========================");
+        //4.Map.entrySet使用iterator遍历key和value..===推荐,尤其是容量大时
+        for (Map.Entry<Object, Object> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + "=" + entry.getValue());
+        }
+
+        for (Object value : map.values()) {
+            System.out.println("value = " + value);
         }
 
     }
