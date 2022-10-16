ea64bf6cf29394753681c003028d3554575da4f9
lingnian <sytsnb@gmail.com>
2022-10-17 03:25:45 +0800

    Security-Demo

---
+++ spring-security/src/main/java/com/syt/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,16 @@
+package com.syt.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sytsn
+ */
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+
+}
