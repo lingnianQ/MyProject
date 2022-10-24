e92686704377dbc76c59fbf1bc54ee22e86b1af4
lingnian <sytsnb@gmail.com>
2022-10-24 17:46:46 +0800

    二分查找与冒泡排序

--- mydemo-00/demo01/src/main/java/algorithm/BinarySearch.java
+++ mydemo-00/demo01/src/main/java/algorithm/BinarySearch.java
@@ -12,7 +12,7 @@ public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {12, 45, 71, 80, 90, 100, 105, 154, 164, 169, 180};
         Arrays.sort(arr);
-//        Arrays.binarySearch();
+        //    参考Arrays.binarySearch()
         BinarySearch binarySearch = new BinarySearch();
         int i = binarySearch.search(arr, 80);
         System.out.println("i = " + i);
@@ -20,20 +20,19 @@ public class BinarySearch {
 
 
     public int search(int[] arr, int t) {
-        int l = 0, r = arr.length - 1, m = 0;
+        int l = 0, r = arr.length - 1, m;
         while (l <= r) {
             //无符号右移--防止边界溢出
             m = (l + r) >>> 1;
-
+            //或者因式分解
+            //m = l - (l / 2 - r / 2);
             if (arr[m] == t) {
                 return m;
             }
             if (arr[m] < t) {
                 l = m + 1;
-                System.out.println(l);
-            } else if (arr[m] > t) {
+            } else {
                 r = m - 1;
-                System.out.println(r);
             }
         }
         return -1;
---
+++ mydemo-00/demo01/src/main/java/algorithm/BubbleSort.java
@@ -0,0 +1,62 @@
+package algorithm;
+
+import java.util.Arrays;
+
+/**
+ * 冒泡排序
+ * 及其优化
+ *
+ * @author sytsnb@gmail.com
+ * @date 2022 2022/10/24 15:06
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 7, 4, 1, 3, 8, 9};
+//        int[] arr = {1, 2, 3, 4, 5, 7, 8, 9};
+//        bubble(arr);
+        bubbleV2(arr);
+    }
+
+    public static void bubbleV2(int[] arr) {
+        int n = arr.length - 1;
+        do {
+            //表示最后一次交换索引位置
+            int last = 0;
+            for (int j = 0; j < n; j++) {
+                System.out.println("比较次数:" + j);
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                    last = j;
+                }
+            }
+            n = last;
+            System.out.println("第" + "轮冒泡:" + Arrays.toString(arr));
+        } while (n != 0);
+    }
+
+
+    public static void bubble(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            boolean swapped = false;
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                System.out.println("比较次数:" + j);
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                    swapped = true;
+                }
+            }
+            System.out.println("第" + i + "轮冒泡:" + Arrays.toString(arr));
+            if (!swapped) {
+                break;
+            }
+        }
+
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
