4aa3b44bf79d76174de53d29b7fb2eaa4f59eb1b
lingnian <sytsnb@gmail.com>
2022-10-25 21:34:01 +0800

    FailFastVsFailSafe

--- mydemo-00/demo01/src/main/java/list/FailFastVsFailSafe.java
+++ mydemo-00/demo01/src/main/java/list/FailFastVsFailSafe.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
+ * ArrayListDemo
+ *
  * @author sytsnb@gmail.com
  * @date 2022 2022/10/25 11:05
  */
 public class FailFastVsFailSafe {
     public static void main(String[] args) {
-        failFast();
-//        failSafe();
+//        failFast();
+        failSafe();
     }
 
 
@@ -42,6 +44,7 @@ public class FailFastVsFailSafe {
         public Student(String name) {
             this.name = name;
         }
+
         String name;
 
         @Override
