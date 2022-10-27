4c1e58017add2493af2cb7a67b0d898088bea29e
lingnian <sytsnb@gmail.com>
2022-10-27 13:24:43 +0800

    二维数组

--- mydemo-00/demo01/src/main/java/array/ArrayDemo1.java
+++ mydemo-00/demo01/src/main/java/array/ArrayDemo1.java
@@ -24,6 +24,13 @@ public class ArrayDemo1 {
 
         System.out.println("----------");
         //遍历
+        for (int[] ints : arr) {
+            for (int anInt : ints) {
+                System.out.print(anInt);
+            }
+            System.out.println();
+        }
+        System.out.println("----------");
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 System.out.print(arr[i][j]);
